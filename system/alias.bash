43892c7f6d16faa0ac425beaf8c33d70c317b3f0
Wangzongcan <wang.zongcan@gmail.com>
2016-06-27 23:07:13 +0800

    emacs 相关 alias

--- system/alias.bash
+++ system/alias.bash
@@ -5,7 +5,10 @@ alias :q="exit"
 
 # emacs
 alias e='emacsclient -t'
-alias en='emacsclient -n'
+alias ec='emacsclient -c'
+alias kemacs='emacsclient -e "(kill-emacs)"'
+alias demacs='$(brew --prefix emacs)/bin/emacs --daemon'
+alias remacs='kemacs & demacs'
 
 # vim
 alias vi=vim
