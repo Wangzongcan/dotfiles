09b63791db2d775582b5fcd934a21f66f3028731
Wangzongcan <wang.zongcan@gmail.com>
2018-11-29 15:49:49 +0800

    修改 setup.sh

--- ruby/config.sh
+++ ruby/config.sh
@@ -2,7 +2,3 @@
 
 alias rake='bundle exec rake'
 alias rspec='bundle exec rspec'
-
-# chruby
-source "/usr/local/share/chruby/chruby.sh"
-source "/usr/local/share/chruby/auto.sh"
--- setup.sh
+++ setup.sh
@@ -4,13 +4,15 @@ DOTFILES_DIR=$(pwd)
 
 function link_dotfiles() {
     local CURRENT_DIR=$1
+    local LINKS_DIR="$CURRENT_DIR/links"
 
-    for FILE_PATH in `find $CURRENT_DIR/links -maxdepth 1 -mindepth 1`; do
-        ln -fs $FILE_PATH "$HOME/.$(basename $FILE_PATH)"
-    done
+    if [ -d $LINKS_DIR ]; then
+        for FILE_PATH in `find $LINKS_DIR -maxdepth 1 -mindepth 1`; do
+            ln -fs $FILE_PATH "$HOME/.$(basename $FILE_PATH)"
+        done
+    fi
 }
 
 for dir in */; do
-    SOURCE_FILE="$DOTFILES_DIR/"$(basename $dir)"/index.sh"
-    [ -f $SOURCE_FILE ] && source $SOURCE_FILE
+    link_dotfiles "$(realpath $dir)"
 done
