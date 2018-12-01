96e8fe0af1a5ebf5881917959d21d65397827817
Wangzongcan <wang.zongcan@gmail.com>
2018-12-01 16:54:28 +0800

    alias bundle exec

--- ruby/config.sh
+++ ruby/config.sh
@@ -3,5 +3,6 @@
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH="/usr/local/lib/ruby/gems/2.5.0/bin:$PATH"
 
+alias be='bundle exec'
 alias rake='bundle exec rake'
 alias rspec='bundle exec rspec'
