5a712afe4e200abee7ae26376e8982ace06edce0
Wangzongcan <wang.zongcan@gmail.com>
2019-02-25 22:23:18 +0800

    设置 bundle alias

--- ruby/env.sh
+++ ruby/env.sh
@@ -3,6 +3,7 @@
 export PATH="/usr/local/opt/ruby@2.5/bin:$PATH"
 export PATH="/usr/local/lib/ruby/gems/2.5.0/bin:$PATH"
 
+alias b='bundle'
 alias be='bundle exec'
 alias rails='bundle exec rails'
 alias rake='bundle exec rake'
