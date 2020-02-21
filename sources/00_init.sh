f41be100322fe812513e4d8510eac4facccf19c5
Wangzongcan <wang.zongcan@gmail.com>
2020-02-21 16:58:59 +0800

    删除 exa

--- sources/00_init.sh
+++ sources/00_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export LANG=zh_CN.UTF-8
 
+alias ll='ls -la'
+
 add-path() {
     if [[ -z $TMUX ]]; then
         export PATH="$1:$PATH"
--- sources/30_exa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-alias ls=exa
-alias l='exa -l'
-alias ll='exa -la'
